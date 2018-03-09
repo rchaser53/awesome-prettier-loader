338c1443dbc06fe6d30871838b6d192835ead9e5
rchaser53 <tayoshizawa29@gmail.com>
2018-03-09 19:45:16 +0900

    fix config file

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         use: [
           'babel-loader',
           {
-            loader: path.resolve(__dirname, 'src/loader.js'),
+            loader: path.resolve(__dirname, 'src/index.js'),
             options: {
               configPath: '.prettierrc',
               ignorePath: '.prettierignore',
