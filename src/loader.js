e8eae6b5d588f1732def8572bb05124dadb84ce5
rchaser53 <tayoshizawa29@gmail.com>
2018-03-09 15:52:13 +0900

    add simple error handling

--- src/loader.js
+++ src/loader.js
@@ -1,25 +1,17 @@
-const fs = require('fs')
+const fs = require('fs');
 const { getOptions } = require('loader-utils');
 const checksum = require('checksum');
-
-const prettier = require("prettier")
+const prettier = require('prettier');
 
 let lastChecksum = {};
 let initialFlg = true;
 let configPrettier;
+let configIgnore;
+
 const pitch = function(remainingRequest, prevRequest, dataInput) {
   const actualPath = remainingRequest.split('!').pop();
   const fileCheckSum = checksum(fs.readFileSync(actualPath));
-
-  if (configPrettier == null) {
-    const { configPath, ignorePath } = getOptions(this);
-    try {
-      const prettierStr = fs.readFileSync(configPath, { encoding: 'utf8' });
-      configPrettier = JSON.parse(prettierStr);
-    } catch (err) {
-      configPrettier = {}
-    }
-  } 
+  initializeConfig(this);
 
   if (lastChecksum[remainingRequest] == null) {
     lastChecksum[remainingRequest] = fileCheckSum;
@@ -29,20 +21,45 @@ const pitch = function(remainingRequest, prevRequest, dataInput) {
   dataInput.fileCheckSum = fileCheckSum;
 }
 
+const initializeConfig = function(context) {
+  if (configPrettier == null || configIgnore == null) {
+    const { configPath, ignorePath } = getOptions(context);
+    try {
+      const prettierStr = fs.readFileSync(configPath, { encoding: 'utf8' });
+      const ignoreStr = fs.readFileSync(ignorePath, { encoding: 'utf8' });
+      configPrettier = JSON.parse(prettierStr);
+      configIgnore = JSON.parse(ignoreStr);
+    } catch (err) {
+      configPrettier = {};
+      configIgnore = {};
+    }
+  }
+} 
+
 const loader = function(sources) {
   const callback = this.async();
-  const { fileCheckSum, remainingRequest } = this.data;
+  const { remainingRequest } = this.data;
 
-  if (fileCheckSum !== lastChecksum[remainingRequest] || initialFlg === true) {
-    const output = prettier.format(fs.readFileSync(remainingRequest, { encoding: 'utf8' }), configPrettier)
-    fs.writeFile(remainingRequest, output, (err) => {
-      delete lastChecksum[remainingRequest];
-      callback(err, sources[0]);
-      initialFlg = false;
-    });
-    return
+  if (isFormat(this)) {
+    try {
+      const output = prettier.format(fs.readFileSync(remainingRequest, { encoding: 'utf8' }), configPrettier);
+      fs.writeFile(remainingRequest, output, (err) => {
+        delete lastChecksum[remainingRequest];
+        callback(err, sources[0]);
+        initialFlg = false;
+      });
+    } catch (err) {
+      console.error(err);
+    }
+
+    return;
   }
   callback(null, sources[0]);
+}
+
+const isFormat = function(context) {
+  const { fileCheckSum, remainingRequest } = context.data;
+  return fileCheckSum !== lastChecksum[remainingRequest] || initialFlg === true
 }
 
 module.exports = {
